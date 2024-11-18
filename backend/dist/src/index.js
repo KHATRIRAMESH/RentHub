1e26036cb00626353e48a362907ec78aba1fa55d
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-11-18 14:42:07 +0545

    cors

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -8,9 +8,10 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:5173",
     "https://rental-ruby.vercel.app",
+    "https://rent-hub-yche.vercel.app"
 ];
 app.use(cors({
-    origin: ["http://localhost:5173", "https://rental-ruby.vercel.app"],
+    origin: ["http://localhost:5173", "https://rental-ruby.vercel.app", "https://rent-hub-yche.vercel.app/"],
     credentials: true,
     methods: "GET, POST, PUT, DELETE",
 }));
